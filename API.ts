c757c9406e2f6d3db95b87801f531786668adb85
Nils Bergemann <nils.bergemann@emplyoee.epicnova.net>
2023-03-16 18:16:52 +0100

    Made the API compatible with both http and https

--- API.ts
+++ API.ts
@@ -1,6 +1,6 @@
 const express = require('express')
+
 var app = express();
-const port = 80
 app.use(express.json());
 
 import { ChatGPTAPI } from 'chatgpt'
@@ -12,7 +12,8 @@ const readFile = promisify(fs.readFile);
 app.post('/', async(req, res) => {
 
   const APIKey = req.headers.authorization.split(' ')[1]
-  if(APIKey == null) { res.send({ "Status": "NoAPIKey", "Message": "No API Key provided" }); return; }
+
+  if(APIKey == null) { res.send("No API Key provided"); return }
 
   const api = new ChatGPTAPI({ apiKey: APIKey })
   
@@ -40,6 +41,39 @@ app.post('/', async(req, res) => {
   
 })
 
-app.listen(port, () => {
-  console.log(`GPT app listening on port ${port}`)
-})
+var Server = null;
+var port = 0
+if(process.argv[2] == "--ssl")
+{ 
+  const https = require('https')
+  const PrivateKeyPath = 'Content/SSL/privkey.pem'
+  const CertificatePath = 'Content/SSL/cert.pem'
+  
+  if(!fs.existsSync(PrivateKeyPath) || !fs.existsSync(CertificatePath)) { console.log("No SSL Certificate found, make sure that SSL/privkey.pem and SSL/cert.pem are present."); process.kill(process.pid, 'SIGINT'); }
+  
+  var PrivateKey = fs.readFileSync('Content/SSL/privkey.pem', 'utf8');
+  var Certificate = fs.readFileSync('Content/SSL/cert.pem', 'utf8');
+  
+  port = 443
+  const Credentials = {key: PrivateKey, cert: Certificate};
+  Server = https.createServer(Credentials, app);
+}
+
+else
+{
+  port = 80
+  const http = require('http')
+  Server = http.createServer(app);
+}
+
+if(Server != null)
+{
+  Server.listen(port, () => {
+    console.log(`GPT app listening on port ${port}`)
+  })
+}
+else
+{
+  console.log("No Server created, due to missing Server object.")
+  process.kill(process.pid, 'SIGINT');
+}
